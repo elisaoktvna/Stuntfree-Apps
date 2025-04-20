6005235cc4338089f9d249fa04d9ccd1b095177e
ell <elisaoktaviana985@gmail.com>
2025-04-20 21:54:34 +0700

    add dashboard-admin

---
+++ StuntingApps/resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+@section('content')
+    <table border="1">
+
+    <tr>
+        <th>halo</th>
+        <th>nama</th>
+        <th>saya</th>
+    </tr>
+    </table>
+@endsection
---
+++ StuntingApps/resources/views/layout/sidebar.blade.php
@@ -0,0 +1,57 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="index.html">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+          <i class="bi bi-menu-button-wide"></i>
+          <span>Orang Tua</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-error-404.html">
+          <i class="bi bi-dash-circle"></i>
+          <span>Data Anak</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-error-404.html">
+          <i class="bi bi-dash-circle"></i>
+          <span>Pengukuran</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-error-404.html">
+          <i class="bi bi-dash-circle"></i>
+          <span>Prediksi</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-error-404.html">
+          <i class="bi bi-dash-circle"></i>
+          <span>Edukasi</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-error-404.html">
+          <i class="bi bi-dash-circle"></i>
+          <span>Rekomendasi Gizi</span>
+        </a>
+      </li>
+
+
+    </ul>
+
+</aside>
