98f9924b9894f60c57e72846f1d374a6826c8f73
Savema <158044968+SavemaAureliandaru@users.noreply.github.com>
2025-05-21 09:42:13 +0700

    card anak dan ortu

--- StuntingApps/resources/views/dashboard.blade.php
+++ StuntingApps/resources/views/dashboard.blade.php
@@ -34,7 +34,7 @@
               </div> --}}
 
               <div class="card-body">
-                <h5 class="card-title">Total Anak <span>| </span></h5>
+                <h5 class="card-title">Total Anak</h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
@@ -61,13 +61,11 @@
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-currency-dollar"></i>
+                    <i class="bi bi-people"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>$3,264</h6>
-                    <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
-                  </div>
+                    <h6>{{ $totalOrtu }}</h6>
+                </div>
                 </div>
               </div>
 
